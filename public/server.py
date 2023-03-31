5b61e6596a0cea26e5bb7de76f8b299462b59995
Ben Puhalski <puhalskib@gmail.com>
2023-03-30 21:29:59 -0400

    finished survey questions, and post requests and dataframe conversion

--- public/server.py
+++ public/server.py
@@ -2,6 +2,12 @@ from flask import Flask
 from markupsafe import escape
 from flask import send_from_directory
 from flask import request, render_template
+import pandas as pd
+import numpy as np
+import pickle
+
+with open('./diabetes_automl.pkl', 'rb') as f:
+    automl = pickle.load(f)
 
 app = Flask(__name__)
 
@@ -18,16 +24,28 @@ def form_example():
     if request.method == 'POST':
         #request_data = request.get_json()
         #SEXVAR = request_data['SEXVAR']
-        SEXVAR = request.form.get('SEXVAR')
-        GENHLTH = request.form.get('GENHLTH')
-        PHYSHLTH = request.form.get('PHYSHLTH')
-        MENTHLTH = request.form.get('MENTHLTH')
+        #print(type(pd.DataFrame.from_dict(request.form.copy().to_dict())))
+        p = pd.DataFrame(request.form.copy().to_dict(), index=[0])
+        #print(pd.DataFrame(request.form.copy().to_dict(), index=[0]))
+        #print(type(pd.DataFrame(request.form.copy().to_dict(), index=[0])))
+        #print(pd.DataFrame(request.form.copy().to_dict(), index=[0]).describe())
+        #print(type(request.form.copy().to_dict()))
+        #print(pd.DataFrame.from_dict(request.form.copy().to_dict()))
+        #print(request.form.copy().to_dict())
+        #SEXVAR = request.form.get('SEXVAR')
+        #GENHLTH = request.form.get('GENHLTH')
+        #PHYSHLTH = request.form.get('PHYSHLTH')
+        #MENTHLTH = request.form.get('MENTHLTH')
+        #p = pd.read_csv('./test_row.csv')
+        p.to_csv('./input_test.csv')
         #framework = request.form.get('framework')
-        return '''
-            <h1>The SEXVAR value is: {}</h1>
-            <h1>The GENHLTH value is: {}</h1>
-            <h1>The PHYSHLTH value is: {}</h1>
-            <h1>The MENTHLTH value is: {}</h1>'''.format(SEXVAR, GENHLTH, PHYSHLTH, MENTHLTH)
+        #return "prediction {}.".format(automl.predict(p))
+        return p.to_string()
+        #'''
+        #    <h1>The SEXVAR value is: {}</h1>
+        #    <h1>The GENHLTH value is: {}</h1>
+        #    <h1>The PHYSHLTH value is: {}</h1>
+        #    <h1>The MENTHLTH value is: {}</h1>'''.format(SEXVAR, GENHLTH, PHYSHLTH, MENTHLTH)
 
     # otherwise handle the GET request
     return send_from_directory('templates', path='diabetes.html')
